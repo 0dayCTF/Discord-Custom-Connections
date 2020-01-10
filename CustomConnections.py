2ecfd2aa3cab89c513effd149609b5cd7ac9f0ba
Ryan Montgomery <44453666+ryanrohypnol@users.noreply.github.com>
2020-01-10 13:52:05 -0500

    Added Python File

---
+++ CustomConnections.py
@@ -0,0 +1,22 @@
+import requests
+
+url = "https://discordapp.com/api/v6/users/@me/connections/battlenet/RANDOMNUMBERS"
+
+payload = "{\"name\": \"CUSTOM COONECTION NAME\",\n\"visibility\": 1\n}"
+headers = {
+    'authorization': "DISCORD TOKEN",
+    'accept-encoding': "gzip, deflate, br",
+    'accept-language': "en-US",
+    'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36",
+    'accept': "*/*",
+    'authority': "discordapp.com",
+    'Content-Type': "application/json",
+    'Cache-Control': "no-cache",
+    'Host': "discordapp.com",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+response = requests.request("PUT", url, data=payload, headers=headers)
+
+print(response.text)
