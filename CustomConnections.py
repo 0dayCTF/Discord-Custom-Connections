8118b91b0288f89d3a3df92ec1bfbb62b49faa96
Ryan Montgomery <44453666+ryanrohypnol@users.noreply.github.com>
2020-02-16 20:20:01 -0500

    Update CustomConnections.py
    Fixed after recent patch

--- CustomConnections.py
+++ CustomConnections.py
@@ -1,10 +1,10 @@
 import requests
-
-url = "https://discordapp.com/api/v6/users/@me/connections/battlenet/RANDOMINTEGER"
-
-payload = "{\"name\": \"CUSTOM CONNECTION NAME\",\n\"visibility\": 1\n}"
+ 
+url = "https://discordapp.com/api/v6/users/@me/connections/skype/RANDOMINTEGER"
+ 
+payload = "{\"name\": \"CUSTOM NAME HERE\",\n\"visibility\": 1\n}"
 headers = {
-    'authorization': "DISCORD TOKEN",
+    'authorization': "YOUR TOKEN HERE",
     'accept-encoding': "gzip, deflate, br",
     'accept-language': "en-US",
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36",
@@ -16,7 +16,7 @@ headers = {
     'Connection': "keep-alive",
     'cache-control': "no-cache"
     }
-
+ 
 response = requests.request("PUT", url, data=payload, headers=headers)
-
+ 
 print(response.text)
